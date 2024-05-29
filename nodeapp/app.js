432b336f3bd3e4f395d86c423aa9327f63d56bae
bashably <bashably8@gmail.com>
2024-05-29 16:26:53 +0530

    Code refactor

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const app = express();
+
+app.listen(80,()=>{
+    console.log("Server is up")
+})
+
 app.get("/",(req,res)=>{
     res.send("Server is up and running")
 })
 
-app.listen(80,()=>{
-    console.log("Server is up")
+app.get("/getMsg",(req,res)=>{
+    res.send("Hi, Gooooooood Morning")
 })
